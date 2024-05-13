4e451343363472040da516f3ae336756f021d1c2
Aubrey Cajayon <aubrey.cajayon@wvsu.edu.ph>
2024-05-13 18:16:37 +0800

    owner layout b

--- resources/views/layouts/owner_layout.blade.php
+++ resources/views/layouts/owner_layout.blade.php
@@ -49,7 +49,8 @@
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center">
                 <img src="assets/img/logo.png" alt="">
-                <span class="d-none d-lg-block text-capitalize"><?php $user = Auth::user();  echo $user['first_name'] . ' ' . $user['last_name']; ?></span>
+                <span class="d-none d-lg-block text-capitalize"><?php $user = Auth::user();
+                echo $user['first_name'] . ' ' . $user['last_name']; ?></span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div><!-- End Logo -->
@@ -76,7 +77,8 @@
 
                 <li class="nav-item dropdown pe-3">
 
-                    <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                    <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
+                        data-bs-toggle="dropdown">
                         <img src="{{ asset('admin_assets/assets/img/profile-img.jpg') }}" alt="Profile"
                             class="rounded-circle">
 
@@ -156,11 +158,11 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse"
                     href="{{ route('order') }}">
-<<<<<<< HEAD
+
                     <i class="bi bi-cart"></i><span>Orders</span><i class="bi bi-chevron-down ms-auto"></i>
-=======
-                    <i class="bi bi-menu-button-wide"></i><span>Orders</span><i class="bi bi-chevron-down ms-auto"></i>
->>>>>>> a915fbea32a3bc7e09aa9b50e14a6089c287e95b
+
+
+
                 </a>
                 <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                     <li>
